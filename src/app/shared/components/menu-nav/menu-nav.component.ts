edc30ecd44e86a151ffaec4058bd8490873eea30
josealejandroprados <josealejandroprados@gmail.com>
2024-09-13 11:07:58 -0300

    agregando logo, iconos para opciones de dibujo y menu de navegacion del sitio

--- src/app/shared/components/menu-nav/menu-nav.component.ts
+++ src/app/shared/components/menu-nav/menu-nav.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu-nav',
   templateUrl: './menu-nav.component.html',
   styleUrls: ['./menu-nav.component.css']
 })
-export class MenuNavComponent {
+export class MenuNavComponent implements OnInit{
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ){}
+
+  logout(){
+    this.auth.logout()
+    .then( () => {
+      console.log('has cerrado sesión');
+
+      this.auth.deleteCredentials();
+
+      this.router.navigate(['/login']);
+
+      setTimeout(() => {
+        window.location.reload();
+      }, 200);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+
+  getUser(){
+    return this.auth.getUser();
+  }
+
+  estalogueado(){
+    return this.auth.estalogueado();
+  }
 
 }
