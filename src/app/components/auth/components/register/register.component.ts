88d40304782f299aff1bda61ead42126156cbd31
josealejandroprados <josealejandroprados@gmail.com>
2024-09-13 11:36:37 -0300

    se implemento register y se agregó modificacion de estilo con css a componente compartido modal

--- src/app/components/auth/components/register/register.component.ts
+++ src/app/components/auth/components/register/register.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ModalComponent } from 'src/app/shared/components/modal/modal.component';
+import { ModalModel } from 'src/app/shared/models/modal.model';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +12,64 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  formRegister = new FormGroup({
+    'email': new FormControl('',[Validators.required, Validators.email]),
+    'password': new FormControl('', Validators.required)
+  });
+
+  modalRegisterUser:ModalModel = {
+    title:'Registrar Usuario',
+    hab_btn: false,
+    textoBodyModal:'',
+    textoBtn:'Aceptar'
+  }
+
+  @ViewChild(ModalComponent) modal!:ModalComponent;
+
+  constructor(
+    private auth:AuthService,
+    private router:Router
+  ){}
+
+  register(){
+    if(this.formRegister.valid){
+      // abrir modal
+      this.modalRegisterUser.textoBodyModal = 'Registrando usuario...';
+      this.modal.abrirModal();
+
+      // console.log(this.formRegister.value);
+
+      this.auth.register(this.formRegister.value)
+      .then(response => {
+        // console.log(response);
+        // registro realizado con exito
+        this.modalRegisterUser.hab_btn = true;
+        this.modalRegisterUser.textoBodyModal = 'Registro realizado con exito';
+
+        // this.router.navigate(['/login']);
+      })
+      .catch(error => {
+        console.log(error);
+        this.modalRegisterUser.hab_btn = true;
+        this.modalRegisterUser.textoBodyModal = '¡Ha ocurrido un error! No se pudo realizar el registro';
+      });
+    }
+  }
+
+  aceptar(){
+    // reinicio variables de modal
+    
+    this.modalRegisterUser.hab_btn = false;
+
+    if(this.modalRegisterUser.textoBodyModal=='Registro realizado con exito'){
+      this.modalRegisterUser.textoBodyModal = '';
+
+      // si el registro es exitoso => redirigir a pagina dibujar
+      this.router.navigate(['/login']);
+    }
+    else{
+      this.modalRegisterUser.textoBodyModal = '';
+    }
+  }
+  
 }
