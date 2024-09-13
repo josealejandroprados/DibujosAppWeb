a103ec17c27d91e04b088873d276720b0447d318
josealejandroprados <josealejandroprados@gmail.com>
2024-09-13 08:49:03 -0300

    comienzo del proyecto, agregando environments

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        "projectId":"TU-PROYECTO",
+        "appId":"TU_APP_ID",
+        "storageBucket":"tu_proyecto.appspot.com",
+        "apiKey":"TU_API_KEY",
+        "authDomain":"tu_proyecto.firebaseapp.com",
+        "messagingSenderId":"TU_SENDER_ID"
+    }
+};
